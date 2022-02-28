5966674dd93ab5fa97f8feff5ed839f5ab49a189
farcek <farcek@gmail.com>
2022-03-01 02:53:06 +0800

    add BooleanItem, ChooseItem

--- src/items.ts
+++ src/items.ts
@@ -17,6 +17,31 @@ export class StringItem implements IConfigItem<string> {
     convert(raw: string, defaut: string) {
         return ('' + raw) || defaut || '';
     }
-
 }
 
+export class ChooseItem implements IConfigItem<string> {
+    constructor(private allowed:string[]) {
+
+    }
+    convert(raw: string, defaut: string) {
+        let v = ('' + raw) || defaut || '';
+        if(this.allowed.includes(v) ) {
+            return v;
+        }
+        throw new Error('not allowed value. allowed values :' + this.allowed.join('; ') )
+    }
+}
+
+export class BooleanItem implements IConfigItem<boolean> {
+    convert(raw: string, defaut: boolean) {
+        let v = ('' + raw).toLowerCase();
+        if (v === 'ok' || v === 'on' || v === '1' || v === 'true' || v === 'yes') {
+            return true;
+        }
+
+        if (v === 'no' || v === 'off' || v === '0' || v === 'false') {
+            return false;
+        }
+        return defaut;
+    }
+}
